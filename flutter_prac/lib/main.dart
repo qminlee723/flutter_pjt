a42c3f5eed99efef01e30b94bcd421b5a178be9d
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-07-17 23:55:30 +0900

    5.6.3 Flexible Widget

--- flutter_prac/lib/main.dart
+++ flutter_prac/lib/main.dart
@@ -278,7 +278,6 @@ import 'package:flutter/material.dart';
 
 // 5.6 배치 관련 위젯
 // 5.6.1 Row 위젯
-
 // void main() {
 //   runApp(RowWidgetExample());
 // }
@@ -326,6 +325,53 @@ void main() {
   runApp(ColumnWidgetExample());
 }
 
+// class ColumnWidgetExample extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: SizedBox(
+//
+//           // 너비를 최대한으로 설정
+//           // 반대축에서 이동할 공간을 제공해주기 위해
+//           width: double.infinity,
+//           child: Column(
+//
+//             // 주축 정렬 지정
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             // 반대축 정렬
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//
+//             children: [
+//               Container(
+//                 height: 50.0,
+//                 width: 50.0,
+//                 color: Colors.red,
+//               ),
+//
+//               // SizedBox는 일반적으로 공백을 생성할 때 사용
+//               const SizedBox(width: 12.0),
+//               Container(
+//                 height: 50.0,
+//                 width: 50.0,
+//                 color: Colors.green,
+//               ),
+//
+//               const SizedBox(width: 12.0),
+//               Container(
+//                 height: 50.0,
+//                 width: 50.0,
+//                 color: Colors.blue,
+//               )
+//             ],
+//           )
+//         )
+//       )
+//     );
+//   }
+// }
+
+// 5.6.3 Flexible 위젯
 class ColumnWidgetExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -333,37 +379,26 @@ class ColumnWidgetExample extends StatelessWidget {
       home: Scaffold(
         body: SizedBox(
 
-          // 너비를 최대한으로 설정
-          // 반대축에서 이동할 공간을 제공해주기 위해
           width: double.infinity,
           child: Column(
-
-            // 주축 정렬 지정
-            mainAxisAlignment: MainAxisAlignment.start,
-            // 반대축 정렬
-            crossAxisAlignment: CrossAxisAlignment.center,
-
             children: [
-              Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.red,
+              // flex는 남은 공간을 차지할 비율
+              // flex값을 제공하지 않으면 기본값은 1
+              Flexible(
+                flex: 1,
+
+                child: Container(
+                  color: Colors.blue,
+                ),
               ),
 
-              // SizedBox는 일반적으로 공백을 생성할 때 사용
-              const SizedBox(width: 12.0),
-              Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.green,
-              ),
+              Flexible(
+                flex: 1,
 
-              const SizedBox(width: 12.0),
-              Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.blue,
-              )
+                child: Container(
+                    color: Colors.red
+                ),
+              ),
             ],
           )
         )
